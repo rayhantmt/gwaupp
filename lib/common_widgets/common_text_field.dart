09a1de037152b27257478861d712bb69bf5fafa2
rayhantmt <rayhantmt@gmail.com>
2026-02-28 02:10:40 +0600

    added the textfield container

---
+++ lib/common_widgets/common_text_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CommonTextField extends StatelessWidget {
+  final String tittle;
+  final bool? obsecure;
+  const CommonTextField({super.key, required this.tittle, this.obsecure});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(tittle,
+        style: GoogleFonts.inter( 
+          fontWeight: FontWeight.w600,
+          fontSize: 16,
+          color: Color(0xff1E1E1E)
+        ),
+        ),
+        SizedBox(height: Get.height*0.03,),
+        Container(
+          height: Get.height*0.05,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: Color(0xffD8D9E0),
+              
+            ),
+            
+          ),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: InputBorder.none
+            )
+          ),
+        )
+      ],
+    );
+  }
+}
