5d5d83b708310d513511e2c46d156a89ef4dd737
rayhantmt <rayhantmt@gmail.com>
2026-02-28 01:59:10 +0600

    designing the log in screen

--- lib/app_modules/log_in/log_in_view.dart
+++ lib/app_modules/log_in/log_in_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:gwaupp/utils/app_images.dart';
 
 class LogInView extends StatelessWidget {
   const LogInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xffFAFAF8),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: Get.height*0.17,),
+            Image.asset(AppImages.logo),
+            Container(
+              height: Get.height*0.5,
+              width: Get.width*0.9,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Color(0xffFFFFFF)
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,3 +1,4 @@
 class AppImages {
   static const String splashScreen='assets/Splash Screen.png';
+  static const String logo='assets/logo.png';
 }
