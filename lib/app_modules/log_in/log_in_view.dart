46bf8f3d36604912aeae5431db00d491a0a5cdc9
rayhantmt <rayhantmt@gmail.com>
2026-02-28 02:02:05 +0600

    added the sign in text

--- lib/app_modules/log_in/log_in_view.dart
+++ lib/app_modules/log_in/log_in_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:gwaupp/utils/app_images.dart';
 
 class LogInView extends StatelessWidget {
@@ -14,12 +15,24 @@ class LogInView extends StatelessWidget {
           children: [
             SizedBox(height: Get.height*0.17,),
             Image.asset(AppImages.logo),
+            SizedBox(height: Get.height*0.05,),
             Container(
               height: Get.height*0.5,
               width: Get.width*0.9,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(12),
                 color: Color(0xffFFFFFF)
+              ),
+              child: Column(
+                children: [
+                  Text('Sign In',
+                  style: GoogleFonts.montserrat( 
+                    fontWeight: FontWeight.w600,
+                    fontSize: 24,
+                    color: Color(0xff1E1E1E)
+                  ),
+                  )
+                ],
               ),
             )
           ],
