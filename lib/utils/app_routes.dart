5302b1b9b8b0b4ebef012d813ea1ed47d76d74ff
rayhantmt <rayhantmt@gmail.com>
2026-03-02 23:11:09 +0600

    added otp page in app pages

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -3,11 +3,14 @@ import 'package:gwaupp/app_modules/create_new_account/create_new_account_binding
 import 'package:gwaupp/app_modules/create_new_account/create_new_account_view.dart';
 import 'package:gwaupp/app_modules/log_in/log_in_binding.dart';
 import 'package:gwaupp/app_modules/log_in/log_in_view.dart';
+import 'package:gwaupp/app_modules/otp_page/otp_page_binding.dart';
+import 'package:gwaupp/app_modules/otp_page/otp_page_view.dart';
 import 'package:gwaupp/utils/app_pages.dart';
 
 class AppRoutes {
   static final pages=[
     GetPage(name: AppPages.login, page: () => LogInView(),binding: LogInBinding()),
     GetPage(name: AppPages.createnewaccount, page: () => CreateNewAccountView(),binding: CreateNewAccountBinding()),
+    GetPage(name: AppPages.otppage, page: () => OtpPageView(),binding: OtpPageBinding()),
   ];
 }
