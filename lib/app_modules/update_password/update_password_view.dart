8cccc655e698c9f5029f68089bc129d9d11731ef
rayhantmt <rayhantmt@gmail.com>
2026-03-03 01:59:55 +0600

    designed the container

--- lib/app_modules/update_password/update_password_view.dart
+++ lib/app_modules/update_password/update_password_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:gwaupp/utils/app_images.dart';
 
 class UpdatePasswordView extends StatelessWidget {
@@ -8,6 +9,27 @@ class UpdatePasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: Get.height*0.06,),
+            Image.asset(AppImages.backIcon,
+            height: Get.height*0.06,
+            ),
+            SizedBox(height: Get.height*0.03,),
+            Container(
+              height: Get.height*0.3,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Color(0xffFFFFFF)
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
