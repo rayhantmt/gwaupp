aa17fb286bdf46cd3d6de8d5082305fb3ea10937
rayhantmt <rayhantmt@gmail.com>
2026-03-03 02:07:41 +0600

    done with the authentication

--- lib/app_modules/update_password/update_password_controller.dart
+++ lib/app_modules/update_password/update_password_controller.dart
@@ -1,5 +1,19 @@
 import 'package:get/get.dart';
 
 class UpdatePasswordController extends GetxController{
+  RxBool isObsecure = true.obs;
+  void toggle() {
+
+    isObsecure.value = !isObsecure.value;
+  
+  }
+  
+
+  RxBool isObsecure2 = true.obs;
+  void toggle2() {
+
+    isObsecure2.value = !isObsecure2.value;
+  
+  }
   
 }
--- lib/app_modules/update_password/update_password_view.dart
+++ lib/app_modules/update_password/update_password_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gwaupp/app_modules/update_password/update_password_controller.dart';
+import 'package:gwaupp/common_widgets/common_button.dart';
+import 'package:gwaupp/common_widgets/common_text_field.dart';
 import 'package:gwaupp/utils/app_images.dart';
 
-class UpdatePasswordView extends StatelessWidget {
+class UpdatePasswordView extends GetView<UpdatePasswordController> {
   const UpdatePasswordView({super.key});
 
   @override
@@ -14,19 +18,64 @@ class UpdatePasswordView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: Get.height*0.06,),
-            Image.asset(AppImages.backIcon,
-            height: Get.height*0.06,
-            ),
-            SizedBox(height: Get.height*0.03,),
+            SizedBox(height: Get.height * 0.06),
+            Image.asset(AppImages.backIcon, height: Get.height * 0.06),
+            SizedBox(height: Get.height * 0.03),
             Container(
-              height: Get.height*0.3,
+              height: Get.height * 0.3,
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(12),
-                color: Color(0xffFFFFFF)
+                color: Color(0xffFFFFFF),
               ),
-            )
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Create New Password',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 24,
+                        color: Color(0xff1E1E1E),
+                      ),
+                    ),
+                    Text(
+                      'Secure your account with a strong 8-digit password.',
+                      style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                        color: Color(0xff6B6B6B),
+                      ),
+                    ),
+                    Obx(
+                      () => CommonTextField(
+                        tittle: 'New Password',
+                        obsecure: controller.isObsecure.value,
+                        suffix: GestureDetector(
+                          onTap: () => controller.toggle(),
+                          child: Icon(Icons.remove_red_eye_outlined),
+                        ),
+                      ),
+                    ),
+                     Obx(
+                      () => CommonTextField(
+                        tittle: 'Confirm New Password',
+                        obsecure: controller.isObsecure2.value,
+                        suffix: GestureDetector(
+                          onTap: () => controller.toggle2(),
+                          child: Icon(Icons.remove_red_eye_outlined),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            CommonButton(tittle: 'Create New Password'),
+            SizedBox(height: Get.height*0.1,)
           ],
         ),
       ),
