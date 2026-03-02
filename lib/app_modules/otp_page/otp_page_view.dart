489cca4a13406b53eb33de0d838cd8693e3fb55a
rayhantmt <rayhantmt@gmail.com>
2026-03-02 23:07:54 +0600

    created the otp page

---
+++ lib/app_modules/otp_page/otp_page_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:gwaupp/utils/app_images.dart';
+
+class OtpPageView extends StatelessWidget {
+  const OtpPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:AppImages.primarycolor
+    );
+  }
+}
