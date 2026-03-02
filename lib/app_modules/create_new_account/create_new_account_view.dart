41d112916ddc9ebaeb9ec2f9582a66c3f0a35ce4
rayhantmt <rayhantmt@gmail.com>
2026-03-02 22:50:03 +0600

    added the rich text widget

--- lib/app_modules/create_new_account/create_new_account_view.dart
+++ lib/app_modules/create_new_account/create_new_account_view.dart
@@ -16,58 +16,91 @@ class CreateNewAccountView extends GetView<CreateNewAccountController> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                height: Get.height*0.06,
-              ),
-              Image.asset(AppImages.backIcon,
-              height: Get.height*0.06,
-              ),
-              SizedBox(height: Get.height*0.03,),
+              SizedBox(height: Get.height * 0.06),
+              Image.asset(AppImages.backIcon, height: Get.height * 0.06),
+              SizedBox(height: Get.height * 0.03),
               Container(
-                height: Get.height*0.3,
+                height: Get.height * 0.3,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12),
-                  color: Color(0xffffffff)
+                  color: Color(0xffffffff),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Create New Account',
-                      style: GoogleFonts.montserrat( 
-                        fontWeight: FontWeight.w600,
-                        fontSize: 24,
-                        color: Color(0xff1E1E1E)
-                      ),
-                      ),
-                      Text('Enter your email and create a strong 8-digit password to create your account.',
-                      style: GoogleFonts.inter( 
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                        color: Color(0xff6B6B6B)
-                      ),
-                      ),
-                      CommonTextField(tittle: 'Email Address', obsecure: false),
-                       Obx(
-                      () => CommonTextField(
-                        tittle: 'Password',
-                        obsecure: controller.isObsecure.value,
-                        suffix: GestureDetector(
-                          onTap: () => controller.toggle(),
-                          child: Icon(Icons.remove_red_eye_outlined),
+                      Text(
+                        'Create New Account',
+                        style: GoogleFonts.montserrat(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24,
+                          color: Color(0xff1E1E1E),
                         ),
                       ),
-                    ),
+                      Text(
+                        'Enter your email and create a strong 8-digit password to create your account.',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color: Color(0xff6B6B6B),
+                        ),
+                      ),
+                      CommonTextField(tittle: 'Email Address', obsecure: false),
+                      Obx(
+                        () => CommonTextField(
+                          tittle: 'Password',
+                          obsecure: controller.isObsecure.value,
+                          suffix: GestureDetector(
+                            onTap: () => controller.toggle(),
+                            child: Icon(Icons.remove_red_eye_outlined),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
-              SizedBox(height: Get.height*0.02,),
-              Text('')
+              SizedBox(height: Get.height * 0.02),
+              Text.rich(
+                textAlign: TextAlign.center,
+                TextSpan(
+                  text: 'By creating an account, you agree to our ',
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: Color(0xff6B6B6B),
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Terms & Conditions.',
+                      style: TextStyle(
+                        color: Color(0xff0F3D2E),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(text: 'Review our'),
+                    TextSpan(
+                      text: 'Privacy Policy',
+                      style: TextStyle(
+                        color: Color(0xff0F3D2E),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' to learn how we protect your information.',
+                      style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Color(0xff6B6B6B),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
