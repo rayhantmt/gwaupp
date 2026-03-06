5408fd90d69f81ee2c5d26ee4cea199308ad951e
rayhantmt <rayhantmt@gmail.com>
2026-03-07 02:04:19 +0600

    added the app bar

--- lib/app_modules/insights/insights_view.dart
+++ lib/app_modules/insights/insights_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:gwaupp/utils/app_images.dart';
 
 class InsightsView extends StatelessWidget {
@@ -8,10 +10,28 @@ class InsightsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
-      body: Column(
-        children: [
-          Text('Insifht')
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: Get.height*0.06,),
+            Row(
+              children: [
+                Icon( Icons.arrow_back_ios_new,
+                color: Colors.black,
+                ),
+                SizedBox(width: Get.width*0.02,),
+                Text('Insights',
+                style: GoogleFonts.montserrat( 
+                  fontWeight: FontWeight.w600,
+                  fontSize: 22,
+                  color: Color(0xff1E1E1E)
+                ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
