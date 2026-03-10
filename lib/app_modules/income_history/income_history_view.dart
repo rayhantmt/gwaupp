44c996d375c9302e02fa9e889dce2aa80e19b087
rayhantmt <rayhantmt@gmail.com>
2026-03-11 05:05:59 +0600

    added the income history view

---
+++ lib/app_modules/income_history/income_history_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:gwaupp/utils/app_images.dart';
+
+class IncomeHistoryView extends StatelessWidget {
+  const IncomeHistoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+      body: Column(
+        children: [
+          SizedBox(height: Get.height*0.06,),
+        ],
+      ),
+    );
+  }
+}
