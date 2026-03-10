4b9ea890f31b9427351cb0b763e05fea436dfae7
rayhantmt <rayhantmt@gmail.com>
2026-03-11 05:08:26 +0600

    added the income history controller

--- lib/app_modules/income_history/income_history_view.dart
+++ lib/app_modules/income_history/income_history_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gwaupp/app_modules/income_history/income_history_controller.dart';
 import 'package:gwaupp/utils/app_images.dart';
 
-class IncomeHistoryView extends StatelessWidget {
+class IncomeHistoryView extends GetView<IncomeHistoryController> {
   const IncomeHistoryView({super.key});
 
   @override
