630fe9ed5be7da051adebe0abce867b3187e13a3
rayhantmt <rayhantmt@gmail.com>
2026-03-11 05:03:18 +0600

    added the scaffold colour for privacy

---
+++ lib/app_modules/privacy_policy/privacy_policy.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:gwaupp/utils/app_images.dart';
+
+class PrivacyPolicy extends StatelessWidget {
+  const PrivacyPolicy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+    );
+  }
+}
