d22aef27a332086303a8f99473eaaf5556b593c7
rayhantmt <rayhantmt@gmail.com>
2026-03-13 01:46:07 +0600

    formatted document

--- lib/app_modules/select_category/select_category_view.dart
+++ lib/app_modules/select_category/select_category_view.dart
@@ -31,9 +31,9 @@ class SelectCategoryView extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: Get.height*0.03,),
+            SizedBox(height: Get.height * 0.03),
             Container(
-              height: Get.height*0.05,
+              height: Get.height * 0.05,
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10000),
@@ -41,27 +41,26 @@ class SelectCategoryView extends StatelessWidget {
                 boxShadow: [
                   BoxShadow(
                     blurRadius: 12,
-                    color: Color(0xff000000).withOpacity(0.1)
-                  )
-                ]
+                    color: Color(0xff000000).withOpacity(0.1),
+                  ),
+                ],
               ),
               child: Row(
                 children: [
-                  SizedBox(width: Get.width*0.05,),
-                  Icon(Icons.search,
-                  color: Color(0xff6B6B6B),
+                  SizedBox(width: Get.width * 0.05),
+                  Icon(Icons.search, color: Color(0xff6B6B6B)),
+                  SizedBox(width: Get.width * 0.05),
+                  Text(
+                    'Select Category',
+                    style: GoogleFonts.inter(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                      color: Color(0xff6B6B6B),
+                    ),
                   ),
-                  SizedBox(width: Get.width*0.05,),
-                  Text('Select Category',
-                  style: GoogleFonts.inter( 
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                    color: Color(0xff6B6B6B)
-                  ),
-                  )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
