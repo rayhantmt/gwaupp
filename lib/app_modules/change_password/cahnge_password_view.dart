f95cc20286c74bef6d2bd85f5cdea5237ba21951
rayhantmt <rayhantmt@gmail.com>
2026-03-14 04:56:40 +0600

    done with the whole frontend

--- lib/app_modules/change_password/cahnge_password_view.dart
+++ lib/app_modules/change_password/cahnge_password_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gwaupp/app_modules/change_password/change_password_controller.dart';
+import 'package:gwaupp/common_widgets/common_button.dart';
+import 'package:gwaupp/common_widgets/common_text_field.dart';
 
-class CahngePasswordView extends StatelessWidget {
+class CahngePasswordView extends GetView<ChangePasswordController> {
   const CahngePasswordView({super.key});
 
   @override
@@ -12,6 +15,7 @@ class CahngePasswordView extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: Get.height*0.06,),
               Row(children: [
@@ -27,7 +31,69 @@ class CahngePasswordView extends StatelessWidget {
                   color: Color(0xff1E1E1E)
                 ),
                 )
-              ],)
+              ],),
+              SizedBox(height: Get.height*0.03,),
+              Container(
+                height: Get.height*0.4,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Set New Password',
+                    style: GoogleFonts.montserrat( 
+                      fontWeight: FontWeight.w600,
+                      fontSize: 24,
+                      color: Color(0xff1E1E1E)
+                    ),
+                    ),
+                    Text('To secure your account,create a strong 8 digit password',
+                    style: GoogleFonts.inter( 
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                      color: Color(0xff6B6B6B)
+                    ),
+                    ),
+                    SizedBox(height: Get.height*0.02,),
+                     Obx(
+                      () => CommonTextField(
+                        tittle: 'Current Password',
+                        obsecure: controller.isObsecure.value,
+                        suffix: GestureDetector(
+                          onTap: () => controller.toggle(),
+                          child: Icon(Icons.remove_red_eye_outlined),
+                        ),
+                      ),
+                    ),
+                     Obx(
+                      () => CommonTextField(
+                        tittle: 'New Password',
+                        obsecure: controller.isObsecure1.value,
+                        suffix: GestureDetector(
+                          onTap: () => controller.toggle1(),
+                          child: Icon(Icons.remove_red_eye_outlined),
+                        ),
+                      ),
+                    ),
+                     Obx(
+                      () => CommonTextField(
+                        tittle: 'Confirm New Password',
+                        obsecure: controller.isObsecure2.value,
+                        suffix: GestureDetector(
+                          onTap: () => controller.toggle2(),
+                          child: Icon(Icons.remove_red_eye_outlined),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            SizedBox(height: Get.height*0.33,),
+              CommonButton(tittle: 'Set New Password'),
+              SizedBox(height: Get.height*0.03,)
             ],
           ),
         ),
--- lib/app_modules/change_password/change_password_controller.dart
+++ lib/app_modules/change_password/change_password_controller.dart
@@ -1,3 +1,23 @@
 import 'package:get/get.dart';
 
-class ChangePasswordController extends GetxController{}
+class ChangePasswordController extends GetxController{
+
+  RxBool isObsecure = true.obs;
+  void toggle() {
+
+    isObsecure.value = !isObsecure.value;
+  
+  }
+  RxBool isObsecure1 = true.obs;
+  void toggle1() {
+
+    isObsecure1.value = !isObsecure1.value;
+  
+  }
+  RxBool isObsecure2 = true.obs;
+  void toggle2() {
+
+    isObsecure2.value = !isObsecure2.value;
+  
+  }
+}
