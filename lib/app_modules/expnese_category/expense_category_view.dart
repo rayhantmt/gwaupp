0dc0111f496a1101f92d4454cb0a7f99d5f3a16e
rayhantmt <rayhantmt@gmail.com>
2026-03-13 06:22:34 +0600

    done with the section

---
+++ lib/app_modules/expnese_category/expense_category_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class  ExpenseCategoryView extends StatelessWidget {
+  const  ExpenseCategoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
