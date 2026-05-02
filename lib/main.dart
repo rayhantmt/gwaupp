0fab5219a454df69c6b379e1c24f12d26aec8e53
rayhantmt <rayhantmt@gmail.com>
2026-05-03 03:48:33 +0600

    added the firebase successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:gwaupp/app_modules/splash/splash_view.dart';
+import 'package:gwaupp/firebase_options.dart';
 import 'package:gwaupp/utils/app_routes.dart';
 import 'package:month_year_picker/month_year_picker.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.currentPlatform,
+  );
   await GetStorage.init();
   runApp(const MyApp());
 }
